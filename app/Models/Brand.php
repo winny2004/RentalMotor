cae8294ff4cc442b493ddce55b43bdd3054df73f
MrCacoe <130184285+ToTenKein@users.noreply.github.com>
2023-08-19 13:24:07 +0700

    Brand

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory;
+    protected $table = 'brands';
+    protected $fillable = [
+        'name',
+        'country',
+        'founded',
+        'logo',
+    ];
 }
